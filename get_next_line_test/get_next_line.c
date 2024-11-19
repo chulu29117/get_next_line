0f9ad86b6d9b35b8366d25d70ddb7c6c884717fb
chuchulu29117 <chuchulu117@gmail.com>
2024-11-19 14:35:19 +0200

    files added

--- get_next_line_test/get_next_line.c
+++ get_next_line_test/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 14:39:02 by clu               #+#    #+#             */
-/*   Updated: 2024/11/19 13:02:08 by clu              ###   ########.fr       */
+/*   Updated: 2024/11/19 14:34:43 by clu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,24 +90,20 @@ char	*set_line(char *line_buffer)
 int	main(void)
 {
 	int 	fd;
-	char	buffer[1024];
-	ssize_t	bytes_read;
+	char	*line;
 	
-	fd = open("text.txt", O_CREAT);
+	fd = open("multi_line.txt", O_RDONLY);
 	if (fd == -1)
 	{
 		perror("Error opening file");
 		return (1);
 	}
-	bytes_read = read(fd, buffer, sizeof(buffer));
-	if (bytes_read == -1)
+	while ((line = get_next_line(fd)) != NULL)
 	{
-		perror("Error reading file");
-		close(fd);
-		return (1);
+		printf("%s", line);
+		free(line);
 	}
-	buffer[bytes_read] = '\0';
-	printf("Read %zd bytes: %s\n", bytes_read, buffer);
+	printf("\n");
 	close(fd);
 	return (0);
 }
